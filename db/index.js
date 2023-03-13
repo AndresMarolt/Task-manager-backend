25bd0bcf23f4572d7cd80ef0bf07df22819b7b5b
AndresMarolt <andresmmarolt@gmail.com>
2023-03-13 01:19:43 -0300

    App working

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 mongoose.Promise = global.Promise
-const mongooseConnect = mongoose.connect('mongodb://localhost:27017/TaskManager', {useNewUrlParser: true, useUnifiedTopology: true})
+const mongooseConnect = mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log("Connected to MongoDB successfully") )
     .catch(() => console.log("Error trying to connect to MongoDB"))
 
